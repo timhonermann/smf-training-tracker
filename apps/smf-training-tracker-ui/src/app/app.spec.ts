15a4f08a2475210a1c7a2989ed583a66162e2fda
Tim Honermann <tim.honermann@windowslive.com>
2025-08-30 14:41:35 +0200

    test: fix failing tests

--- apps/smf-training-tracker-ui/src/app/app.spec.ts
+++ apps/smf-training-tracker-ui/src/app/app.spec.ts
@@ -1,20 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome smf-training-tracker-ui'
-    );
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
