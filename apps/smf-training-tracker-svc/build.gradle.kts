90d3e1ffcb005e0e688d0d746e2d5ee31c5b4f3e
Tim Honermann <tim.honermann@windowslive.com>
2026-07-10 23:47:57 +0200

    build: nx 23 upgrade

--- apps/smf-training-tracker-svc/build.gradle.kts
+++ apps/smf-training-tracker-svc/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
   id("java")
-  id("dev.nx.gradle.project-graph") version ("0.1.8")
+  id("dev.nx.gradle.project-graph") version ("0.1.22")
   id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
 }
@@ -41,8 +44,16 @@ tasks.withType<Test> {
   useJUnitPlatform()
 }
 
-tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-  mainClass.set("ch.smf.smf_training_tracker_svc.SmfTrainingTrackerServiceApplication")
+tasks.named<BootJar>("bootJar") {
+  mainClass.set(
+    "ch.smf.smf_training_tracker_svc.SmfTrainingTrackerServiceApplication"
+  )
+}
+
+tasks.named<BootRun>("bootRun") {
+  mainClass.set(
+    "ch.smf.smf_training_tracker_svc.SmfTrainingTrackerServiceApplication"
+  )
 }
 
 allprojects {
